8b39f873fdb42fbd79830aefd42110d10f00c4fe
Sebastian Hermida <sebas00@gmail.com>
2010-01-18 07:57:49 -0500

    single lcd numbers now work

--- LCD-numbers/test_lcd.py
+++ LCD-numbers/test_lcd.py
@@ -1,10 +1,42 @@
 import unittest
 
-def lcd(n):
-    return '\n|\n\n|\n\n'
+SPACE = ' '
+PIPE  = '|'
+DASH  = '-'
+TEMPLATE = ' %s\n%s %s\n %s\n%s %s\n %s'
 
-class TestLCD(unittest.TestCase):
+numbers = {
+    0: [DASH, PIPE, PIPE, SPACE, PIPE, PIPE, DASH],
+    1: [SPACE, SPACE, PIPE, SPACE, SPACE, PIPE, SPACE],
+    2: [DASH, SPACE, PIPE, DASH, PIPE, SPACE, DASH],
+    3: [DASH, SPACE, PIPE, DASH, SPACE, PIPE, DASH],
+}
+
+def lcd(n):
+    num = ' ' + numbers[n][0] + '\n'
+    num += numbers[n][1] + ' ' + numbers[n][2] + '\n'
+    num += ' ' + numbers[n][3] + '\n'
+    num += numbers[n][4] + ' ' + numbers[n][5] + '\n'
+    num += ' ' + numbers[n][6]
+    return num
+
+class TestLCDWithSingleNumber(unittest.TestCase):
+    def test_display_zero(self):
+        expected_result = TEMPLATE % (DASH, PIPE, PIPE, SPACE, PIPE, PIPE, DASH)
+        self.assertEquals(expected_result, lcd(0))
     
     def test_display_one(self):
-        self.assertEquals('\n|\n\n|\n\n', lcd(1))
+        expected_result = TEMPLATE % (SPACE, SPACE, PIPE, SPACE, SPACE, PIPE, SPACE) 
+        self.assertEquals(expected_result, lcd(1))
         
+    def test_display_two(self):
+        expected_result = TEMPLATE % (DASH, SPACE, PIPE, DASH, PIPE, SPACE, DASH)
+        self.assertEquals(expected_result, lcd(2))
+
+    def test_display_three(self):
+        expected_result = TEMPLATE % (DASH, SPACE, PIPE, DASH, SPACE, PIPE, DASH)
+        self.assertEquals(expected_result, lcd(3))
+        
+
+if __name__ == '__main__':
+    print lcd(2)
